8bb522cc88053015a6f420f986d296d6e7a99861
Dieghoatc <dieghoatc@gmail.com>
2023-04-08 15:04:24 +1000

    feat: config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Dieghoatc.github.io",
-  base: "/Personal-Website",
+  site: "https://dieghoatc.github.io",
+  base: "/personalweb",
 });
