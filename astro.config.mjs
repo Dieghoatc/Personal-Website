7b35a557905da15dcc0cda41850bab03394d5c90
Dieghoatc <dieghoatc@gmail.com>
2023-04-08 22:13:33 +1000

    feat: add config versel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dieghoatc.com",
   base: "/personalweb",
+  output: "server",
+  adapter: vercel()
 });
